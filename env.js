941f68cf82294db843585c3f8314d0ef34eae006
amrthameen <thameen.amr@gmail.com>
2018-11-11 09:46:00 -0500

    added env.js

--- env.js
+++ env.js
@@ -1,6 +1,7 @@
 module.exports = {
-    cred
-    accountSid = 'AC991dcdb1fdb56bda733a6d3384e67706',
-    authToken = 'daa8e0a9df1eb042833f7f0d82506a59',
-    phoneNumber = '+18557550509'
+    credentials : {
+        accountSid : 'AC991dcdb1fdb56bda733a6d3384e67706',
+        authToken : 'daa8e0a9df1eb042833f7f0d82506a59',
+        phoneNumber : '+18557550509'
+    }
 }
