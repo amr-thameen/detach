45b4161e15064990db16e8131967165711a333ea
Amr Thameen <thameen.amr@gmail.com>
2018-11-11 09:48:16 -0500

    Delete env.js

--- env.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    credentials : {
-        accountSid : 'AC991dcdb1fdb56bda733a6d3384e67706',
-        authToken : 'daa8e0a9df1eb042833f7f0d82506a59',
-        phoneNumber : '+18557550509'
-    }
-}
